2a15324a0903abd673171a636c0b1e5139f4b6da
Julius <juliuszh@stanford.edu>
2023-10-25 13:29:41 -0700

    Rpc gateway

---
+++ src/overlay/rpc_gateway.rs
@@ -0,0 +1,29 @@
+use std::collections::HashMap;
+
+use super::peer::PeerID;
+
+// When an application is constructed, it needs to register with an RpcGateway struct to receive rpc calls.
+pub trait RpcGateway { 
+    fn register(&mut self, peer_id: PeerID, write_queue: RpcRequestWriteQueue);
+    fn remove(&mut self, peer_id: PeerID);
+}
+
+pub struct TestRpcGateway {
+    write_queues: HashMap<PeerID, RpcRequestWriteQueue>,
+}
+
+impl TestRpcGateway {
+    pub fn new() -> Self {
+        TestRpcGateway { write_queues: HashMap::new() }
+    }
+}
+
+impl RpcGateway for TestRpcGateway {
+    fn register(&mut self, peer_id: PeerID, write_queue: RpcRequestWriteQueue) {
+        self.write_queues[peer_id] = write_queue;
+    }
+
+    fn remove(&mut self, peer_id: PeerID) {
+        self.write_queues.remove(peer_id);
+    }
+}
