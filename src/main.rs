bd8899f9c8f70a275f92ee36e338e260e3bbda24
Julius <juliuszh@stanford.edu>
2023-10-24 20:49:45 -0700

    working work queue example

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
+use std::{
+    borrow::BorrowMut,
+    collections::VecDeque,
+    ops::{Deref, DerefMut},
+    rc::{Rc, Weak},
+    sync::{Arc, Mutex},
+};
 
-use std::{rc::{Rc, Weak}, sync::{Arc, Mutex}, collections::VecDeque, borrow::BorrowMut, ops::DerefMut};
-
-type Callback<'a> = Arc<dyn FnMut() + 'a>;
+type Callback = Arc<dyn FnMut()>;
 
 struct State {
     value: usize,
 }
-
-
 
 impl State {
     pub fn new() -> Self {
@@ -18,23 +21,8 @@ impl State {
         self.value += 1;
     }
 
-    // pub fn add_to_queue(&'_ mut self, work_queue: &mut WorkQueue) {
-    //     let strong = Arc::new(Mutex::new(self));
-    //     let weak = Arc::downgrade(&strong); 
-
-    //     let callback = move || {
-    //         match weak.upgrade() {
-    //             None => {},
-    //             Some(state) =>{
-    //                 state.lock().unwrap().incr_one();
-    //             },
-    //         };
-    //     };
-    //     work_queue.add_task(Arc::new(callback));
-    // }
-
-    pub fn add_to_queue<'a>(&'a mut self, work_queue: &mut WorkQueue<'a>) {
-        let  strong = Arc::new(Mutex::new(self)).clone();
+    pub fn add_to_queue(this: &Arc<Mutex<Self>>, work_queue: &mut WorkQueue) -> () {
+        let strong = this.clone();
         // let mut strong = self.clone();
         let weak = Arc::downgrade(&strong);
 
@@ -42,45 +30,45 @@ impl State {
             match weak.upgrade() {
                 None => {
                     println!("State does not exist.")
-                },
-                Some( _state) =>{
-                    let mut state = _state.lock().unwrap();
+                }
+                Some(_state) => {
+                    let mut state: std::sync::MutexGuard<'_, State> = _state.lock().unwrap();
                     state.incr_one();
                     println!("State with value {}", state.value);
-                },
+                }
             };
         });
         work_queue.add_task(callback);
     }
 
-    pub fn to_callback<'a>(&'a mut self) -> impl FnMut() + 'a  {
+    pub fn to_callback<'a>(&'a mut self) -> impl FnMut() + 'a {
         let strong: Arc<Mutex<&mut State>> = Arc::new(Mutex::new(self));
-        let weak = Arc::downgrade(&strong); 
+        let weak = Arc::downgrade(&strong);
 
         let a = move || {
             match weak.upgrade() {
-                None => {},
-                Some(state) =>{
+                None => {}
+                Some(state) => {
                     state.lock().unwrap().incr_one();
-                },
+                }
             };
         };
         a
-    } 
+    }
 }
 
-struct WorkQueue<'a>
-{
-    tasks: VecDeque<Callback<'a>> 
-    // TODO: add clock, etc
+struct WorkQueue {
+    tasks: VecDeque<Callback>, // TODO: add clock, etc
 }
 
-impl<'a> WorkQueue<'a> {
+impl WorkQueue {
     pub fn new() -> Self {
-        WorkQueue { tasks: VecDeque::<Callback>::new() }
+        WorkQueue {
+            tasks: VecDeque::<Callback>::new(),
+        }
     }
 
-    pub fn add_task(&mut self, callback: Callback<'a>) -> () {
+    pub fn add_task(&mut self, callback: Callback) -> () {
         self.tasks.push_back(callback);
     }
 
@@ -89,7 +77,7 @@ impl<'a> WorkQueue<'a> {
             match self.tasks.pop_front() {
                 Some(mut callback) => {
                     Arc::get_mut(&mut callback).unwrap()();
-                }, 
+                }
                 None => break,
             }
         }
@@ -98,21 +86,30 @@ impl<'a> WorkQueue<'a> {
 
 fn main() {
     let mut work_queue = WorkQueue::new();
-    let state1 = Arc::new(Mutex::new(State::new()));
 
-    state1.lock().unwrap().add_to_queue(&mut work_queue);
+    {
+        let  state1 = Arc::new(Mutex::new(State::new()));
+
+        work_queue.execute_task();
+
+        state1.lock().unwrap().incr_one();
+        state1.lock().unwrap().incr_one();
+        // let pt2 = pt.clone();
+
+        State::add_to_queue(&state1, &mut work_queue);
+    }
     work_queue.execute_task();
-    
 
+    // state1.add_to_queue(&mut work_queue)
 
     // let state = Arc::new(State{value: 0});
     // let weak = Arc::downgrade(&state);
-    // 
+    //
     // let strong = weak.upgrade();
     // assert!(strong.is_some());
-    // 
+    //
     // drop(strong);
     // drop(state);
-// 
+    //
     // assert!(weak.upgrade().is_none());
 }
